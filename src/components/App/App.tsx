46329782c6115d4368c8ceb963ba5955f4b14fe4
Edward <vecheryaedik@gmail.com>
2025-07-31 18:17:26 +0300

    v-0.1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,2 +1,11 @@
-import { useState } from "react"
-import css from "./App.module.css"
+import { useState } from 'react';
+import css from './App.module.css';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import Loader from '../Loader/Loader';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import MovieModal from '../MovieModal/MovieModal';
+import SearchBar from '../SearchBar/SearchBar';
+import css from './App.module.css';
+import toast, { Toaster } from 'react-hot-toast';
+import { type Movie } from '../../types/movie';
+import { fetchMovies } from '../../services/movieService';
